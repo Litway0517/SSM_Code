92040d80a57e5101022862105f35b7fe1fe04c30
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-30 21:17:20 +0800

    spring_ioc spring的开发步骤: 配置spring依赖 -> 创建Bean对象(所有的java源码其实都是Bean 比如DAO层 service层 不单纯指的是pojo domain entity...) -> *配置xml文件 -> 使用spring代码通过id值反射获取Bean

--- itheima_spring_ioc/src/test/java/com/itheima/dao/impl/UserDaoImplTest.java
+++ itheima_spring_ioc/src/test/java/com/itheima/dao/impl/UserDaoImplTest.java
@@ -1,5 +1,6 @@
 package com.itheima.dao.impl;
 
+import com.itheima.dao.UserDao;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,8 +11,10 @@ public class UserDaoImplTest {
     @Test
     public void save() {
 
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationConfig");
-
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // 提供Bean的id参数(id就是唯一值) 然后spring框架会反射得到该对象
+        UserDao userDao = (UserDao) app.getBean("userDao");
+        userDao.save();
 
     }
 }
