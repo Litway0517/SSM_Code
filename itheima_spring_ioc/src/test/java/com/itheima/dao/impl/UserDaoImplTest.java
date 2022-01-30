015e667f2c199895a6bc6802ac895e86462fbaab
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-30 21:09:29 +0800

    spring_ioc 测试UserDaoImplTest

---
+++ itheima_spring_ioc/src/test/java/com/itheima/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,17 @@
+package com.itheima.dao.impl;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class UserDaoImplTest {
+
+    @Test
+    public void save() {
+
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationConfig");
+
+
+    }
+}
