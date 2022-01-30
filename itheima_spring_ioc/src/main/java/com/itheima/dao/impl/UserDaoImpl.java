64517738af0455cea1a986904fc98d4e81994b23
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-30 20:48:01 +0800

    spring_ioc UserDao的实现类: UserDaoImpl

---
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.UserDao;
+
+/**
+ * 用户DAOImpl
+ *
+ * @author DELL_
+ * @date 2022/01/30
+ */
+public class UserDaoImpl implements UserDao {
+
+
+    /**
+     * 保存
+     */
+    public void save() {
+        System.out.println("save running......");
+    }
+}
