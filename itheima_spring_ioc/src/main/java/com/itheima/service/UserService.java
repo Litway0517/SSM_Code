b9a4c6fbe557e20aef5140915106fb0f332dd23d
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-31 11:43:59 +0800

    spring_ioc 增加UserService层

---
+++ itheima_spring_ioc/src/main/java/com/itheima/service/UserService.java
@@ -0,0 +1,15 @@
+package com.itheima.service;
+
+/**
+ * 用户服务
+ *
+ * @author DELL_
+ * @date 2022/01/31
+ */
+public interface UserService {
+    /**
+     * 保存
+     */
+    public void save();
+
+}
