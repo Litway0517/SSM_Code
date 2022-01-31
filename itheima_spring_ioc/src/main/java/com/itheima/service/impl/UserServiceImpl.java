53ec182897ea9d7d84d7c824835a8a76fa194b86
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-31 11:47:03 +0800

    spring_ioc 通过spring创建DAO层 实现解耦

--- itheima_spring_ioc/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.itheima.service.impl;
 
+import com.itheima.dao.UserDao;
 import com.itheima.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserServiceImpl implements UserService {
 
@@ -8,6 +10,12 @@ public class UserServiceImpl implements UserService {
      * 保存
      */
     public void save() {
-        
+        /*
+            这里是spring帮我们产生的DAO层 不需要我们在new UserDaoImpl();对象.
+            当需要改动的时候只需要改配置文件内的代码而不需要改源码 -> 实现解耦
+         */
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = (UserDao) app.getBean("userDao3");
+        userDao.save();
     }
 }
