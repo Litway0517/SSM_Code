c746a3c0156e3c47d4820b7b2449523a92211ba9
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-31 12:13:25 +0800

    spring_ioc 通过配置的appC.xml文件 spring方式获取service层 和DAO层一样的

--- itheima_spring_ioc/src/main/java/com/itheima/controller/UserController.java
+++ itheima_spring_ioc/src/main/java/com/itheima/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.itheima.controller;
 
+import com.itheima.service.UserService;
+import com.itheima.service.impl.UserServiceImpl;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 用户控制器
+ *
+ * @author DELL_
+ * @date 2022/01/31
+ */
 public class UserController {
+
+    public static void main(String[] args) {
+        // 原始的实例化方式
+        /*
+            UserServiceImpl userServiceImpl = new UserServiceImpl();
+            userServiceImpl.save();
+        */
+
+
+        // 使用spring解耦
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserServiceImpl userServiceImpl = (UserServiceImpl) app.getBean("userService");
+        userServiceImpl.save();
+
+    }
 }
