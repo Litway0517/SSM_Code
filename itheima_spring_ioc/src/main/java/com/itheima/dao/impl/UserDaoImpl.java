be1142db76c15a526a3b6947a391ec5302add19a
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-31 10:45:20 +0800

    spring_ioc 增加两个方法 在appC.xml配置文件中指定作为创建时的方法 和 销毁时的方法

--- itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -24,4 +24,21 @@ public class UserDaoImpl implements UserDao {
     public void save() {
         System.out.println("save running......");
     }
+
+    /**
+     * 初始化
+     */
+    public void init() {
+        System.out.println("初始化方法......");
+    }
+
+
+    /**
+     * 销毁
+     */
+    public void destroy() {
+        System.out.println("销毁方法......");
+    }
+
+
 }
