81ca4e2fd44ba14b9a9e2fcb0b5a913bb464c745
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-03 12:50:23 +0800

    spring_ioc_anno Impl类

---
+++ itheima_spring_ioc/src/main/java/com/itheima/dao/impl/BookDaoImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.BookDao;
+
+/**
+ * 书DAOImpl
+ *
+ * @author DELL_
+ * @date 2022/02/03
+ */
+public class BookDaoImpl implements BookDao {
+
+
+    /**
+     * 被书书id
+     */
+    public void getBookByBookId() {
+        System.out.println("查找图书...");
+    }
+}
