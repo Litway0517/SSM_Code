63c9b286a0837543bec02133ec6d48790237bc9e
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-03 14:48:36 +0800

    spring_ioc_anno 数组数据类型的数据注入

--- itheima_spring_ioc/src/main/java/com/itheima/controller/BookController.java
+++ itheima_spring_ioc/src/main/java/com/itheima/controller/BookController.java
@@ -1,9 +1,15 @@
 package com.itheima.controller;
 
+import com.itheima.dao.impl.BookDaoImpl;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class BookController {
 
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookDaoImpl bookDaoImpl = (BookDaoImpl) app.getBean("bookDaoImpl");
+        bookDaoImpl.getBookByBookId();
 
-
-
+    }
 
 }
