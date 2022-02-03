9ffe4b13680d07a6f5fe84ca07ad2924d68f4d51
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-03 15:29:34 +0800

    spring_ioc_anno 通过配置文件加载数据源的配置

--- itheima_spring_ioc_anno/src/test/java/com/itheima/DataSourceTest.java
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/DataSourceTest.java
@@ -5,8 +5,10 @@ import com.alibaba.druid.pool.DruidPooledConnection;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * 数据源测试
@@ -39,6 +41,11 @@ public class DataSourceTest {
     @Test
     public void testDruid() throws SQLException {
         DruidDataSource druidDataSource = new DruidDataSource();
+
+        /*
+            设置上必要的连接参数, 但是这样写上的话就和源码耦合在一起了. 后期如果更换数据库则必须修改本部分的源码.
+            接下来就抽取配置信息成为properties
+         */
         druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
         druidDataSource.setUrl("jdbc:mysql://localhost:3306/test");
         druidDataSource.setUsername("root");
@@ -48,6 +55,29 @@ public class DataSourceTest {
         System.out.println(connection);
         connection.close();
         // com.mysql.jdbc.JDBC4Connection@18ef96
+    }
+
+
+    @Test
+    public void testC3P0ByProperties() throws PropertyVetoException, SQLException {
+        ComboPooledDataSource c3p0 = new ComboPooledDataSource();
+
+        // 使用ResourceBundle读取配置文件 -> ResourceBundle的基路径就是 resources 配置文件的路径
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String driver = rb.getString("jdbc.driver");
+        String url = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+
+        // 加载读取到的配置
+        c3p0.setDriverClass(driver);
+        c3p0.setJdbcUrl(url);
+        c3p0.setUser(username);
+        c3p0.setPassword(password);
+
+        Connection connection = c3p0.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
 
