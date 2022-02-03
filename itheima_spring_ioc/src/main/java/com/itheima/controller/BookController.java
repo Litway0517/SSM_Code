796d7baa722cf3046bb8820215df9f55a928dbb8
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-03 15:11:45 +0800

    spring_ioc

--- itheima_spring_ioc/src/main/java/com/itheima/controller/BookController.java
+++ itheima_spring_ioc/src/main/java/com/itheima/controller/BookController.java
@@ -3,6 +3,12 @@ package com.itheima.controller;
 import com.itheima.dao.impl.BookDaoImpl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * 本控制器
+ *
+ * @author DELL_
+ * @date 2022/02/03
+ */
 public class BookController {
 
     public static void main(String[] args) {
