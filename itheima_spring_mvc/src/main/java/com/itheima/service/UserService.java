303d734cd0175966ca01298e41f06c0168e0e757
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 21:19:11 +0800

    spring_ioc_mvc

---
+++ itheima_spring_mvc/src/main/java/com/itheima/service/UserService.java
@@ -0,0 +1,15 @@
+package com.itheima.service;
+
+/**
+ * 用户服务
+ *
+ * @author DELL_
+ * @date 2022/02/04
+ */
+public interface UserService {
+
+    /**
+     * 保存
+     */
+    public void save();
+}
---
+++ itheima_spring_mvc/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.UserService;
+
+/**
+ * 用户服务Impl
+ *
+ * @author DELL_
+ * @date 2022/02/04
+ */
+public class UserServiceImpl implements UserService {
+
+    /**
+     * 保存
+     */
+    public void save() {
+
+    }
+}
