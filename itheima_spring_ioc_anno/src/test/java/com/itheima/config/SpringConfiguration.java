49398eeded865ccba7d259e65ba0aca279c21145
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 13:21:39 +0800

    spring_ioc_anno Spring核心配置类

---
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/config/SpringConfiguration.java
@@ -0,0 +1,14 @@
+package com.itheima.config;
+
+
+// 加上注解 @Configuration 标名此类是Spring配置类
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+
+
+
+
+}
