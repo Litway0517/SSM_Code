e2b90aa15481c27d1eefa878f34c38f45921ac72
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 11:18:23 +0800

    spring_ioc_anno 通过setter方式在service层注入DAO层

--- itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -1,4 +1,28 @@
 package com.itheima.service.impl;
 
-public class UserServiceImpl {
+import com.itheima.dao.UserDao;
+import com.itheima.service.UserService;
+
+/**
+ * 用户服务Impl
+ *
+ * @author DELL_
+ * @date 2022/02/04
+ */
+public class UserServiceImpl implements UserService {
+
+    // 创建一个DAO的实例, 采用setter方式注入DAO层. 这样实例化service层就会自动有DAO层
+    private UserDao userDao;
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    /**
+     * 保存
+     */
+    public void save() {
+        userDao.save();
+
+    }
 }
