2a0b45b9b671e4c8982c26107ad76de7ed576207
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 12:33:48 +0800

    spring_ioc_anno 当容器中的实例唯一时 只是用@Autowired注解就可以注入 但是不唯一时需要再加上@Qualifier("id")注解标识唯一性

--- itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -29,11 +29,17 @@ public class UserServiceImpl implements UserService {
         <property name="userDao" ref="userDao" />
         不再使用xml的方式 + setter 方式注入DAO. 使用注解
      */
+
+
+    /*
+        如果成员变量userDao上面仅有@Autowired注解 那么spring就会按照这个变量的类型到容器中去寻找.
+        此时userDaoImpl上面有@Repository注解(有注解spring就会帮助程序员创建这个Bean, 容器中就会有一个UserDaoImpl对象),
+            那么就会注入到这个变量中. 但是如果spring容器中有两个UserDaoImpl实例对象, spring就不知道具体将哪一个注入给userDao
+            成员变量. 因此需要加上@Qualifier("id标识") 来确定.
+     */
     @Autowired
     @Qualifier("userDao")
     private UserDao userDao;
-
-
 
     /**
      * 保存
