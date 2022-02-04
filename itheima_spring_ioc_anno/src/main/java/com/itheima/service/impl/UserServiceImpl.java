6e659c20ee49d93d7c6100863bace17e265dffb3
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 12:08:25 +0800

    spring_ioc_anno 类声明上加注解@Component("id标识")  需要注入的成员变量声明上加注解@Autowired @Qualifier("该成员变量的id标识")

--- itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.itheima.service.impl;
 
 import com.itheima.dao.UserDao;
 import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 /**
  * 用户服务Impl
@@ -9,10 +12,24 @@ import com.itheima.service.UserService;
  * @author DELL_
  * @date 2022/02/04
  */
+
+/*
+    <bean id="userService" class="com.itheima.service.impl.UserServiceImpl" scope="prototype" />
+    同样的使用注解代替上面的xml配置信息
+ */
+@Component("userService")
 public class UserServiceImpl implements UserService {
 
     // 创建一个DAO的实例, 采用setter方式注入DAO层. 这样实例化service层就会自动有DAO层
+
+    /*
+        <property name="userDao" ref="userDao" />
+        不再使用xml的方式 + setter 方式注入DAO. 使用注解
+     */
+    @Autowired
+    @Qualifier("userDao")
     private UserDao userDao;
+
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
