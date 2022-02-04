5c1d74000b50d31b2b1085742a282305ee9f4ec5
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 18:26:43 +0800

    spring_ioc_anno 测试不使用app.xml配置文件

--- itheima_spring_ioc_anno/src/test/java/com/itheima/controller/UserController.java
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.itheima.controller;
 
+import com.itheima.config.SpringConfiguration;
 import com.itheima.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -20,8 +22,15 @@ public class UserController {
         // 手动关闭容器, 就会出现了销毁方法
         app.close();
 
+        System.out.println("===============================");
 
 
+        // 此时没有app.xml配置文件了. 使用以下代码测试
+        AnnotationConfigApplicationContext app2 = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+        UserServiceImpl userService2 = app2.getBean(UserServiceImpl.class);
+        userService2.save();
+
+        app2.close();
     }
 
 
