4bced6df3679c3a35b2a835eb7f101ac0e210287
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-04 11:15:28 +0800

    spring_ioc_anno service层接口

---
+++ itheima_spring_ioc_anno/src/main/java/com/itheima/service/UserService.java
@@ -0,0 +1,18 @@
+package com.itheima.service;
+
+/**
+ * 用户服务
+ *
+ * @author DELL_
+ * @date 2022/02/04
+ */
+public interface UserService {
+
+
+    /**
+     * 保存
+     */
+    public void save();
+
+
+}
