1ac6e1e9c50d7ac0554de828e4541d43c8be9720
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-05 11:28:11 +0800

    spring_ioc_mvc 导入spring-web坐标 然后使用继承的WebApplicationContextUtils获取spring容器 再使用就好了

--- itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
+++ itheima_spring_mvc/src/main/java/com/itheima/web/UserServlet.java
@@ -2,9 +2,10 @@ package com.itheima.web;
 
 
 import com.itheima.service.impl.UserServiceImpl;
-import com.itheima.utils.WebApplicationContextUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,11 +24,19 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // 最原始的方法
+        // ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+
         ServletContext servletContext = req.getServletContext();
 
         // 直接使用工具类取出spring容器, 而不需要记住名称.
         // ApplicationContext app = (ClassPathXmlApplicationContext) servletContext.getAttribute("app");
 
+        // 这个是手动封装的工具. 但是spring-web已经集成了改工具. 因此直接用就好了
+        // ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+        // spring-web中的工具包
         ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         UserServiceImpl userServiceImpl = app.getBean(UserServiceImpl.class);
         userServiceImpl.save();
