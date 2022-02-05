00a18d9af03d709e3122a42765bbe3a04da14e3b
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-05 16:26:10 +0800

    spring_ioc_mvc 当@RequestMapping加载类声明上面时作为基地址 因此需要注意返回的地址

--- itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
+++ itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
@@ -6,11 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 // 将控制器放到spring容器中
 @Controller
+
+// 增该类的路由基地址
+@RequestMapping(name = "/user")
 public class UserController {
 
 
     /*
         @RequestMapping注解是指: 请求映射. 即浏览器的请求到达时, 如果路径是quick. 那么就访问这个方法
+        @RequestMapping注解能够用在方法上, 也能够用在类名上.
+
+        - 仅使用在方法上是:
+            如save()方法上的quick -> http://ip:port/quick
+
+        - 在类名称上使用@RequestMapping("/xxx"). 并且方法名字上也是用@RequestMapping("/quick")
+            那么路由地址是 -> http://ip:port/xxx/quick
+            即是类名称上面的地址 拼接 方法名字上的地址构成的.
      */
     @RequestMapping("/quick")
     public String save() {
@@ -18,10 +29,14 @@ public class UserController {
 
         /*
             直接return到需要跳转的界面的名称就行. 比之前的开发简单多了.
+                这里面return是相对地址.
+                例如: 请求地址是http://ip:port/abc/
+                    那么return的地址时http://ip:port/abc/success.jsp
+                    所以这个地址有可能不正确
             之前是:
                 req.getDispatcher("/quick/...").forward(req, resp);
          */
-        return "success.jsp";
+        return "/success.jsp";
     }
 
 
