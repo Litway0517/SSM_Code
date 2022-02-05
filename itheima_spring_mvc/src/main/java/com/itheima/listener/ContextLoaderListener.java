4217a81ddf4616ae02d386373a710529fd30ac6f
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-05 10:46:31 +0800

    spring_ioc_mvc 解耦加载spring容器时读取配置文件的名称 改成读取web.xml配置文件中的初始化参数

--- itheima_spring_mvc/src/main/java/com/itheima/listener/ContextLoaderListener.java
+++ itheima_spring_mvc/src/main/java/com/itheima/listener/ContextLoaderListener.java
@@ -22,9 +22,18 @@ public class ContextLoaderListener implements ServletContextListener {
      * @param servletContextEvent servlet上下文事件
      */
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // 将spring上下文对象存储到最大的域中, 即application域
+        /*
+            这里面加载的spring配置文件仍然是写固定的, 因此需要解耦. 配置到web.xml文件下, 作为一个初始化参数.
+            如果以后更换了配置文件的名称, 那么不需要更改本处的源码, 而只需要去更改web.xml中的配置文件名称.
+            之前需要改两处, 现在只需要更改一处.
+         */
+        // ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // 读取web.xml中的全局初始化参数
         ServletContext servletContext = servletContextEvent.getServletContext();
+        String springConfiguration = servletContext.getInitParameter("contextConfigLocation");
+        ApplicationContext app = new ClassPathXmlApplicationContext(springConfiguration);
+        // 将spring上下文对象存储到最大的域中, 即application域
         servletContext.setAttribute("app", app);
 
         System.out.println("spring容器创建完毕.....");
