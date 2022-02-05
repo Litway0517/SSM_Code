a0bfba02c9207bbe45c14fea1f80cdb92fb791c7
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-05 10:31:43 +0800

    spring_ioc_mvc 增加一个 上下文启动监听器ContextLoaderListener 类 当监听到服务器启动时执行初始化方法 就会创建spring容器并存储到最大的域 application域中

---
+++ itheima_spring_mvc/src/main/java/com/itheima/listener/ContextLoaderListener.java
@@ -0,0 +1,39 @@
+package com.itheima.listener;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * 上下文加载器侦听器
+ *
+ * @author DELL_
+ * @date 2022/02/05
+ */
+public class ContextLoaderListener implements ServletContextListener {
+
+
+    /**
+     * 环境初始化
+     *
+     * @param servletContextEvent servlet上下文事件
+     */
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // 将spring上下文对象存储到最大的域中, 即application域
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.setAttribute("app", app);
+    }
+
+    /**
+     * 环境破坏
+     *
+     * @param servletContextEvent servlet上下文事件
+     */
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
