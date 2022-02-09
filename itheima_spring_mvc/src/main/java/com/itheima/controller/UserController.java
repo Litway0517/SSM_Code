5648805f4c1c92407866932afee4bfa84de6a4d6
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-09 17:32:55 +0800

    spring_ioc_mvc 原生的Servlet API

--- itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
+++ itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,6 +26,23 @@ import java.util.List;
 // 增该类的路由基地址
 @RequestMapping("/user")
 public class UserController {
+
+
+
+    /*
+        原生的Servlet API
+     */
+    @RequestMapping("/quick19")
+    @ResponseBody
+    public void getServletApi(HttpServletRequest request,HttpServletResponse response,
+                              HttpSession session)
+    {
+        System.out.println(request);
+        System.out.println(response);
+        System.out.println(session);
+    }
+
+
 
     /*
         配置自定义的类型转换器
