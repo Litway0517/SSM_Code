681d2d3df339e44a7094b94ab6c34032aae5b1b1
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-10 14:14:19 +0800

    spring_jdbc

---
+++ itheima_spring_jdbc/src/main/java/com/itheima/jdbc/JdbcTemplateTest.java
@@ -0,0 +1,16 @@
+package com.itheima.jdbc;
+
+/**
+ * jdbc模板测试
+ *
+ * @author DELL_
+ * @date 2022/02/10
+ */
+public class JdbcTemplateTest {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
