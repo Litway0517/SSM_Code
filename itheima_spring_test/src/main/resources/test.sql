8e96058c15921b68cdd846d9c3f9fa44eab5dfb1
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-10 21:54:12 +0800

    spring_test 数据库名称改一下

--- itheima_spring_test/src/main/resources/test.sql
+++ itheima_spring_test/src/main/resources/test.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Ultimate v12.09 (64 bit)
-MySQL - 5.7.24-log : Database - test
+MySQL - 5.7.24-log : Database - spring_test
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.7.24-log : Database - test
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`spring_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `test`;
+USE `spring_test`;
 
 /*Table structure for table `sys_role` */
 
