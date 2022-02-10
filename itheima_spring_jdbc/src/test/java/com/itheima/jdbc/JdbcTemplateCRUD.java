46818fc035ea7ff89525ebdea5de7f4387b3c565
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-10 16:10:54 +0800

    spring_jdbc 查询单个对象和多个对象 查询到的结果使用BeanPropertyRowMapper<元素类型>(元素字节码)接收 特殊参数的查询

--- itheima_spring_jdbc/src/test/java/com/itheima/jdbc/JdbcTemplateCRUD.java
+++ itheima_spring_jdbc/src/test/java/com/itheima/jdbc/JdbcTemplateCRUD.java
@@ -1,12 +1,16 @@
 package com.itheima.jdbc;
 
 
+import com.itheima.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
@@ -14,6 +18,33 @@ public class JdbcTemplateCRUD {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+
+    // 查询特殊对象
+    @Test
+    public void testQuerySingle() {
+        Long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM `account`", Long.class);
+        System.out.println(count);
+    }
+
+
+
+    // 查询单个对象
+    @Test
+    public void testQueryForOne() {
+        Account result = jdbcTemplate.queryForObject("SELECT * FROM `account` WHERE `id`=?",
+                new BeanPropertyRowMapper<Account>(Account.class), 2);
+        System.out.println(result);
+    }
+
+
+    // 查询多个对象
+    @Test
+    public void testQueryAll() {
+        List<Account> result = jdbcTemplate.query("SELECT * FROM `account`", new BeanPropertyRowMapper<Account>(Account.class));
+        System.out.println(result);
+    }
+
 
 
     // 测试删除
