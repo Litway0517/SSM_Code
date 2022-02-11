e4603e3ba99cbad9579a2d5af92b6210e6229f58
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-11 20:41:20 +0800

    spring_test 增加domain注释

--- itheima_spring_test/src/main/java/com/itheima/domain/Role.java
+++ itheima_spring_test/src/main/java/com/itheima/domain/Role.java
@@ -1,35 +1,85 @@
 package com.itheima.domain;
 
+/**
+ * 角色
+ *
+ * @author DELL_
+ * @date 2022/02/11
+ */
 public class Role {
 
+    /**
+     * id
+     */
     private Long id;
+    /**
+     * 角色名
+     */
     private String roleName;
+    /**
+     * 角色desc
+     */
     private String roleDesc;
 
+    /**
+     * 得到id
+     *
+     * @return {@link Long}
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * 组id
+     *
+     * @param id id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * 角色名字
+     *
+     * @return {@link String}
+     */
     public String getRoleName() {
         return roleName;
     }
 
+    /**
+     * 设置角色名称
+     *
+     * @param roleName 角色名
+     */
     public void setRoleName(String roleName) {
         this.roleName = roleName;
     }
 
+    /**
+     * 获取角色desc
+     *
+     * @return {@link String}
+     */
     public String getRoleDesc() {
         return roleDesc;
     }
 
+    /**
+     * 设置角色desc
+     *
+     * @param roleDesc 角色desc
+     */
     public void setRoleDesc(String roleDesc) {
         this.roleDesc = roleDesc;
     }
 
+    /**
+     * 字符串
+     *
+     * @return {@link String}
+     */
     @Override
     public String toString() {
         return "Role{" +
--- itheima_spring_test/src/main/java/com/itheima/domain/User.java
+++ itheima_spring_test/src/main/java/com/itheima/domain/User.java
@@ -1,53 +1,129 @@
 package com.itheima.domain;
 
+/**
+ * 用户
+ *
+ * @author DELL_
+ * @date 2022/02/11
+ */
 public class User {
 
+    /**
+     * id
+     */
     private Long id;
+    /**
+     * 用户名
+     */
     private String username;
+    /**
+     * 电子邮件
+     */
     private String email;
+    /**
+     * 密码
+     */
     private String password;
+    /**
+     * 全国矿工工会电话
+     */
     private String phoneNum;
 
+    /**
+     * 得到id
+     *
+     * @return {@link Long}
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * 组id
+     *
+     * @param id id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * 获得用户名
+     *
+     * @return {@link String}
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * 设置用户名
+     *
+     * @param username 用户名
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * 获得电子邮件
+     *
+     * @return {@link String}
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * 设置电子邮件
+     *
+     * @param email 电子邮件
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * 得到密码
+     *
+     * @return {@link String}
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * 得到电话num
+     *
+     * @return {@link String}
+     */
     public String getPhoneNum() {
         return phoneNum;
     }
 
+    /**
+     * 设置手机num
+     *
+     * @param phoneNum 全国矿工工会电话
+     */
     public void setPhoneNum(String phoneNum) {
         this.phoneNum = phoneNum;
     }
 
+    /**
+     * 字符串
+     *
+     * @return {@link String}
+     */
     @Override
     public String toString() {
         return "User{" +
