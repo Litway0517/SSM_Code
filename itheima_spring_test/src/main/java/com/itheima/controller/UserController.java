10a65025b4754d2e6409cfdf5e33a0501ecb9824
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-12 18:04:12 +0800

    spring_test UserController web层

---
+++ itheima_spring_test/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.itheima.controller;
+
+
+import com.itheima.domain.User;
+import com.itheima.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @RequestMapping("/list")
+    public ModelAndView list(User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        List<User> userList = userServiceImpl.list(user);
+        modelAndView.addObject("userList", userList);
+
+        // 设置试图
+        modelAndView.setViewName("user-list");
+        return modelAndView;
+    }
+}
