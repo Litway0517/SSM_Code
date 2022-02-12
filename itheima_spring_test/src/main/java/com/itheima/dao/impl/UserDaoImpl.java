0eced2f24adcade1bb6383bf21aa2f0d8afe84e4
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-12 21:32:57 +0800

    spring_test DAO分步完成插入步骤 先插入用户(但是这个id怎么获取? 需要重写底层的逻辑 update(PreparedStatementCreator psc, GeneratedKeyHolder gkl) 有些麻烦) 接口并实现

--- itheima_spring_test/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_test/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -5,8 +5,13 @@ import com.itheima.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -42,15 +47,34 @@ public class UserDaoImpl implements UserDao {
      *
      * @param user 用户
      */
-    public Long addUser(User user) {
-
+    public Long addUser(final User user) {
         /*
             如果存在并发情况, 那么同时插入了很多user. 而此时并没有获取到插入user的id, 所以就会错误的生成用户觉得关系表.
             所以这个查询语句不完全正确.
          */
-        String sql = "INSERT INTO `spring_test`.`sys_user` VALUES(?,?,?,?,?)";
-        jdbcTemplate.update(sql, null, user.getUsername(), user.getEmail(), user.getPassword(), user.getPhoneNum());
-        return 0L;
+        // String sql = "INSERT INTO `spring_test`.`sys_user` VALUES(?,?,?,?,?)";
+        // jdbcTemplate.update(sql, null, user.getUsername(), user.getEmail(), user.getPassword(), user.getPhoneNum());
+
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                // 使用原始的jdbc完成PreparedStatement的组建
+                String sql = "INSERT INTO `spring_test`.`sys_user` VALUES(?,?,?,?,?)";
+                PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+                // 设置参数
+                ps.setObject(1, null);
+                ps.setString(2, user.getUsername());
+                ps.setString(3, user.getEmail());
+                ps.setString(4, user.getPassword());
+                ps.setString(5, user.getPhoneNum());
+                return ps;
+            }
+        };
+
+        GeneratedKeyHolder gkl = new GeneratedKeyHolder();
+        // 使用另一个update方法! update(PreparedStatementCreator psc, KeyHolder generatedKeyHolder )
+        jdbcTemplate.update(psc, gkl);
+        return gkl.getKey().longValue();
     }
 
     /**
@@ -60,8 +84,10 @@ public class UserDaoImpl implements UserDao {
      * @param roleIds 角色id
      */
     public void addUserRoleRel(Long userId, Long[] roleIds) {
-        String sql = "INSERT INTO `spring_test`.`sys_user_role` VALUES(?,?)";
-        jdbcTemplate.update(sql, userId, roleIds);
+        for (Long roleId : roleIds) {
+            String sql = "INSERT INTO `spring_test`.`sys_user_role` VALUES(?,?)";
+            jdbcTemplate.update(sql, userId, roleId);
+        }
     }
 
 
