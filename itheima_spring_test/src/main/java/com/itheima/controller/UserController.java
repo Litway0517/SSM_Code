c36415e6a02d1c0bd411076a34652949d9ae215d
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-12 20:40:50 +0800

    spring_test 增加方法: 当点击 [新建用户] 按钮时 需要先查询数据库中的所有角色 这样才是动态展示

--- itheima_spring_test/src/main/java/com/itheima/controller/UserController.java
+++ itheima_spring_test/src/main/java/com/itheima/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.itheima.controller;
 
 
+import com.itheima.domain.Role;
 import com.itheima.domain.User;
+import com.itheima.service.RoleService;
+import com.itheima.service.impl.RoleServiceImpl;
 import com.itheima.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +20,11 @@ public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
+    @Autowired
+    private RoleServiceImpl roleServiceImpl;
+
+
+    // 用户界面展示
     @RequestMapping("/list")
     public ModelAndView list() {
         ModelAndView modelAndView = new ModelAndView();
@@ -27,4 +35,28 @@ public class UserController {
         modelAndView.setViewName("user-list");
         return modelAndView;
     }
+
+
+    // 用户进入增加用户界面之后, 需要动态展示角色列表
+    @RequestMapping("/addUI")
+    public ModelAndView addUI() {
+        ModelAndView modelAndView = new ModelAndView();
+        List<Role> roleList = roleServiceImpl.list();
+        modelAndView.addObject("roleList", roleList);
+
+        // 跳转路径
+        modelAndView.setViewName("user-add");
+        return modelAndView;
+
+    }
+
+
+    // 实现添加用户
+    @RequestMapping("/add")
+    public String add(User user, Long[] roleIds) {
+        userServiceImpl.add(user, roleIds);
+
+        return "redirect:/user/list";
+    }
+
 }
