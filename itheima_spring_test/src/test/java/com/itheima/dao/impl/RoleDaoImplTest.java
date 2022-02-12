0bc1a6b94862216bdfeddf020bf51c9f8690eaae
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-12 11:58:37 +0800

    spring_test DAO层测试

--- itheima_spring_test/src/test/java/com/itheima/dao/impl/RoleDaoImplTest.java
+++ itheima_spring_test/src/test/java/com/itheima/dao/impl/RoleDaoImplTest.java
@@ -1,12 +1,26 @@
 package com.itheima.dao.impl;
 
+import com.itheima.domain.Role;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class RoleDaoImplTest {
+
+    @Autowired
+    private RoleDaoImpl roleDaoImpl;
+
 
     @Test
     public void add() {
+
+        roleDaoImpl.add(new Role(null, "testName", "testDesc"));
     }
 }
