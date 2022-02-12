6a6546858cd4c1572ecc1625631ef7e96e8cdca4
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-12 20:00:42 +0800

    spring_test 业务层调用

--- itheima_spring_test/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ itheima_spring_test/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.itheima.service.impl;
 
 import com.itheima.dao.impl.UserDaoImpl;
+import com.itheima.domain.Role;
 import com.itheima.domain.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,16 @@ public class UserServiceImpl implements UserService {
      */
     public List<User> list() {
         List<User> userList = userDaoImpl.list();
+
+        /*
+            因为每个用户对应的角色不一定是一个, 所以需要用List<Role>封装. 但是这样封装的话, 就不能一次性使用sql返回结果.
+            所以, 在这里service层, 再进行一次查询! 通过用户id, 将角色id查询出来. 进而查询详细信息.
+         */
+        for (User user : userList) {
+            List<Role> roleByUserId = userDaoImpl.getRoleByUserId(user.getId());
+            user.setRoles(roleByUserId);
+        }
+
         return userList;
     }
 }
