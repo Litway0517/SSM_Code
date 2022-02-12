d49242a43ef67b1234a001590283a9814f187b8a
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-12 21:16:03 +0800

    spring_test 新增角色 接口并实现

--- itheima_spring_test/src/main/java/com/itheima/dao/UserDao.java
+++ itheima_spring_test/src/main/java/com/itheima/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.itheima.dao;
 
-import com.itheima.domain.Role;
 import com.itheima.domain.User;
 
 import java.util.List;
@@ -21,5 +20,19 @@ public interface UserDao {
     public List<User> list();
 
 
+    /**
+     * 添加用户
+     *
+     * @param user 用户
+     */
+    public Long addUser(User user);
 
+
+    /**
+     * 添加用户角色rel
+     *
+     * @param userId  用户id
+     * @param roleIds 角色id
+     */
+    public void addUserRoleRel(Long userId, Long[] roleIds);
 }
--- itheima_spring_test/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_test/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -1,7 +1,6 @@
 package com.itheima.dao.impl;
 
 import com.itheima.dao.UserDao;
-import com.itheima.domain.Role;
 import com.itheima.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -31,9 +30,39 @@ public class UserDaoImpl implements UserDao {
      * @return {@link List}<{@link User}>
      */
     public List<User> list() {
-        String sql ="SELECT * FROM `sys_user`";
+        String sql ="SELECT * FROM `spring_test`.`sys_user`";
         BeanPropertyRowMapper<User> result = new BeanPropertyRowMapper<User>(User.class);
         List<User> userList = jdbcTemplate.query(sql, result);
         return userList;
     }
+
+
+    /**
+     * 添加用户
+     *
+     * @param user 用户
+     */
+    public Long addUser(User user) {
+
+        /*
+            如果存在并发情况, 那么同时插入了很多user. 而此时并没有获取到插入user的id, 所以就会错误的生成用户觉得关系表.
+            所以这个查询语句不完全正确.
+         */
+        String sql = "INSERT INTO `spring_test`.`sys_user` VALUES(?,?,?,?,?)";
+        jdbcTemplate.update(sql, null, user.getUsername(), user.getEmail(), user.getPassword(), user.getPhoneNum());
+        return 0L;
+    }
+
+    /**
+     * 添加用户角色rel
+     *
+     * @param userId  用户id
+     * @param roleIds 角色id
+     */
+    public void addUserRoleRel(Long userId, Long[] roleIds) {
+        String sql = "INSERT INTO `spring_test`.`sys_user_role` VALUES(?,?)";
+        jdbcTemplate.update(sql, userId, roleIds);
+    }
+
+
 }
