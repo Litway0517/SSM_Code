8e934918ef75cd8a1b83f89d21d776e02c90ab21
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-16 21:55:51 +0800

    spring_tx_annotation 使用注解 配置组件自动扫描

--- itheima_spring_tx_annotation/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ itheima_spring_tx_annotation/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -2,7 +2,10 @@ package com.itheima.service.impl;
 
 import com.itheima.dao.AccountDao;
 import com.itheima.service.AccountService;
+import org.springframework.stereotype.Service;
 
+
+@Service("accountService")
 public class AccountServiceImpl implements AccountService {
 
     private AccountDao accountDao;
