bf192bb8828d143caea1981baf1bf047764e469d
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-16 11:47:31 +0800

    spring_aop @Aspect注解标识一个类是 切面类(springmvc中的说法)  @Before("切点表达式") @Before注解就说明了是前置通知 其次切点表达式也说明了为谁增强 结合@Aspect就能够确定了

--- itheima_spring_aop/src/main/java/com/itheima/proxy/annotation/MyAspect.java
+++ itheima_spring_aop/src/main/java/com/itheima/proxy/annotation/MyAspect.java
@@ -1,9 +1,25 @@
 package com.itheima.proxy.annotation;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+// 使用@Component告诉spring容器, 本类的创建权交给spring容器管理
+@Component("myAspect")
+
+
+// 添加@Aspect注解 标识此类是  切面类
+@Aspect
 public class MyAspect {
 
+
+    /*
+        配置前置增强
+        首先, @Before注解的名称就决定了这个是 前置通知.
+        value用来表述 切点表达式.
+     */
+    @Before(value = "execution(void com.itheima.proxy.annotation.*.*(..))")
     public void before() {
         System.out.println("前置增强: 执行目标方法之前的 增强方法");
     }
