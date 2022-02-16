400f1eb7c5a757414458adea20d4b6233bbfa8bf
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-16 21:37:49 +0800

    spring_tx_annotation 从spring_tx模块中导入过来的itheima包

---
+++ itheima_spring_tx_annotation/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,21 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.AccountDao;
+import com.itheima.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+
+    private AccountDao accountDao;
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public void transfer(String outMan, String inMan, double money) {
+        accountDao.out(outMan,money);
+
+        // 产生错误
+        int i = 1 / 0;
+
+        accountDao.in(inMan,money);
+    }
+}
