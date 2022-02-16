21e15e049d8fc8fa1b1d7f3032beb306e336e794
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-16 16:46:53 +0800

    spring_tx 制造一个错误

--- itheima_spring_tx/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ itheima_spring_tx/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -12,6 +12,10 @@ public class AccountServiceImpl implements AccountService {
 
     public void transfer(String outMan, String inMan, double money) {
         accountDao.out(outMan,money);
+
+        // 产生错误
+        int i = 1 / 0;
+
         accountDao.in(inMan,money);
     }
 }
