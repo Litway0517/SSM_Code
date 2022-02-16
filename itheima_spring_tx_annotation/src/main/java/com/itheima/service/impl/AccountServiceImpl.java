97e070f4a53187112127df4cbeb678e1fdd435ad
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-16 22:15:04 +0800

    spring_tx_annotation @Transactional(内部可填参数) 注解是用来 业务类上面 或者 业务方法上面. 用来标识该方法需要进行事务控制. 事务控制的细节由[内部可填参数]标名

--- itheima_spring_tx_annotation/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ itheima_spring_tx_annotation/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -3,9 +3,19 @@ package com.itheima.service.impl;
 import com.itheima.dao.AccountDao;
 import com.itheima.service.AccountService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("accountService")
+
+/*
+    增加@Transactional注解, 表示该业务类下面的所有方法均使用, 该事务控制. 以及控制的细节, 即参数, 也遵循这个注解.
+    但是, 当某个方法上面, 也有了具体的@Transactional时, 就以方法上面的注解为主.
+ */
+@Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRED)
 public class AccountServiceImpl implements AccountService {
 
     private AccountDao accountDao;
@@ -13,6 +23,10 @@ public class AccountServiceImpl implements AccountService {
         this.accountDao = accountDao;
     }
 
+
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRED,
+            timeout = TransactionDefinition.TIMEOUT_DEFAULT)
     public void transfer(String outMan, String inMan, double money) {
         accountDao.out(outMan,money);
 
