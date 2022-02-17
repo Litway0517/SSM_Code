0237c43014a8459345d3234013e0b48f9f8a881f
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-17 15:25:04 +0800

    spring_mybatis 测试插入操作  mybatis默认没有提交 需要手动提交

--- itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
+++ itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
@@ -15,6 +15,37 @@ public class MyBatisTest {
 
 
 
+
+    @Test
+    public void save() throws IOException {
+
+        User user = new User();
+        user.setUsername("alien");
+        user.setPassword("5678");
+
+        // 获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+
+        // 获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        // 获得session会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 执行操作  参数: namespace + id
+        int insert = sqlSession.insert("userMapper.save", user);
+
+        // mybatis的事务默认是关闭的, 也就是说, 当有数据更新的时候, 需要手动提交
+        sqlSession.commit();
+
+        System.out.println(insert);
+
+
+        // 释放资源
+        sqlSession.close();
+    }
+
+
     @Test
     public void findAll() throws IOException {
 
