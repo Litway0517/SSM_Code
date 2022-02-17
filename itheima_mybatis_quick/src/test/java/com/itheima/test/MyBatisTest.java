4112fc387cc79410237efce9952a900b17f5195e
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-17 15:51:36 +0800

    spring_mybatis

--- itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
+++ itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
@@ -16,10 +16,6 @@ public class MyBatisTest {
 
     @Test
     public void delete() throws IOException {
-
-        User user = new User();
-        user.setId(1);
-
         // 获得核心配置文件
         InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
 
@@ -29,14 +25,13 @@ public class MyBatisTest {
         // 获得session会话对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
-        // 执行操作  参数: namespace + id. 后面还需要给予插入的实体
-        int delete = sqlSession.delete("userMapper.delete", user);
+        // 执行操作  参数: namespace + id. 这里面给user实体也可以
+        int delete = sqlSession.delete("userMapper.delete", 1);
 
         // mybatis的事务默认是关闭的, 也就是说, 当有数据更新的时候, 需要手动提交
         sqlSession.commit();
 
         System.out.println(delete);
-
 
         // 释放资源
         sqlSession.close();
@@ -46,7 +41,6 @@ public class MyBatisTest {
 
     @Test
     public void update() throws IOException {
-
         User user = new User();
         user.setId(11);
         user.setUsername("lucy");
@@ -69,7 +63,6 @@ public class MyBatisTest {
 
         System.out.println(update);
 
-
         // 释放资源
         sqlSession.close();
     }
@@ -78,7 +71,6 @@ public class MyBatisTest {
 
     @Test
     public void save() throws IOException {
-
         User user = new User();
         user.setUsername("alien");
         user.setPassword("5678");
@@ -100,7 +92,6 @@ public class MyBatisTest {
 
         System.out.println(insert);
 
-
         // 释放资源
         sqlSession.close();
     }
@@ -108,7 +99,6 @@ public class MyBatisTest {
 
     @Test
     public void findAll() throws IOException {
-
         // 获得核心配置文件
         InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
 
@@ -123,11 +113,7 @@ public class MyBatisTest {
 
         System.out.println(userList);
 
-
         // 释放资源
         sqlSession.close();
     }
-
-
-
 }
