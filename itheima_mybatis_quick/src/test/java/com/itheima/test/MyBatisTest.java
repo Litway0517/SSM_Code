a28669e8a74a5a89d432a51cc49e87e60291f6c1
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-17 15:39:42 +0800

    spring_mybatis 更细操作 parameterType元素用来指出参数的类型 测试方法

--- itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
+++ itheima_mybatis_quick/src/test/java/com/itheima/test/MyBatisTest.java
@@ -14,6 +14,36 @@ import java.util.List;
 public class MyBatisTest {
 
 
+    @Test
+    public void update() throws IOException {
+
+        User user = new User();
+        user.setId(11);
+        user.setUsername("lucy");
+        user.setPassword("5678");
+
+        // 获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+
+        // 获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        // 获得session会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 执行操作  参数: namespace + id. 后面还需要给予插入的实体
+        int update = sqlSession.update("userMapper.update", user);
+
+        // mybatis的事务默认是关闭的, 也就是说, 当有数据更新的时候, 需要手动提交
+        sqlSession.commit();
+
+        System.out.println(update);
+
+
+        // 释放资源
+        sqlSession.close();
+    }
+
 
 
     @Test
