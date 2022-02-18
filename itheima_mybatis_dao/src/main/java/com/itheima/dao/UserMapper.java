81d6da77302a1baaa1f8ebbfb38c4229c81e5eb9
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-18 19:56:51 +0800

    mybatis UserMapper接口

--- itheima_mybatis_dao/src/main/java/com/itheima/dao/UserMapper.java
+++ itheima_mybatis_dao/src/main/java/com/itheima/dao/UserMapper.java
@@ -2,11 +2,32 @@ package com.itheima.dao;
 
 import com.itheima.entity.User;
 
+import java.io.IOException;
 import java.util.List;
 
+/**
+ * 用户映射器
+ *
+ * @author DELL_
+ * @date 2022/02/17
+ */
 public interface UserMapper {
 
-    public List<User> findAll();
+    /**
+     * 找到所有
+     *
+     * @return {@link List}<{@link User}>
+     * @throws IOException IO异常
+     */
+    public List<User> findAll() throws IOException;
+
+
+    /**
+     * 发现通过id
+     *
+     * @return {@link User}
+     */
+    public User findById(int id);
 
 
 
