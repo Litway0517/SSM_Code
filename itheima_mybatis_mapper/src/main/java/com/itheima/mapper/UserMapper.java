3085e83367ee321afed4898ce311223dd84170b3
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-18 21:15:42 +0800

    mybatis_mapper 增加查询方法 参数是 List<Integer>类型的

--- itheima_mybatis_mapper/src/main/java/com/itheima/mapper/UserMapper.java
+++ itheima_mybatis_mapper/src/main/java/com/itheima/mapper/UserMapper.java
@@ -4,9 +4,28 @@ import com.itheima.entity.User;
 
 import java.util.List;
 
+/**
+ * 用户映射器
+ *
+ * @author DELL_
+ * @date 2022/02/18
+ */
 public interface UserMapper {
 
+    /**
+     * 找到条件
+     *
+     * @param user 用户
+     * @return {@link List}<{@link User}>
+     */
     public List<User> findByCondition(User user);
+
+    /**
+     * 发现由ids
+     *
+     * @return {@link List}<{@link User}>
+     */
+    public List<User> findByIds(List<Integer> ids);
 
 
 
