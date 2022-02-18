b048e03807da1d8168bb79d2ebf21c0dbbc6664a
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-18 20:48:27 +0800

    mybatis_mapper 增加接口方法

---
+++ itheima_mybatis_mapper/src/main/java/com/itheima/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.itheima.mapper;
+
+import com.itheima.entity.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    public List<User> findByCondition(User user);
+
+
+
+
+}
