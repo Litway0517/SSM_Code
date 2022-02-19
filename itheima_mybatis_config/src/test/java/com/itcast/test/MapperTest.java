1fe308bbbc9d3dcfbe64e870ebb78fbc334c4510
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-19 11:24:46 +0800

    mybatis_config 分页插件的使用 功能比较强大 注意PageInfo类的使用 很好用

--- itheima_mybatis_config/src/test/java/com/itcast/test/MapperTest.java
+++ itheima_mybatis_config/src/test/java/com/itcast/test/MapperTest.java
@@ -1,5 +1,7 @@
 package com.itcast.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.itcast.entity.User;
 import com.itcast.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
@@ -23,10 +25,36 @@ public class MapperTest {
         //获得MyBatis框架生成的UserMapper接口的实现类
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
 
-        List<User> all = userMapper.findAll();
-        for (User user : all) {
+        // 设置分页数据. 当前页码号+每一页显示的条数. 即, 第五页, 每页三条 -> 最后得到的是13, 14, 15这三条数据
+        PageHelper.startPage(3, 3);
+
+        List<User> userList = userMapper.findAll();
+        for (User user : userList) {
             System.out.println(user);
         }
+
+        /*
+            获取与分页相关的参数
+            这个是根据上面的查询到的结果进行推算的.
+            当前页码, 其实就是上面传递的参数 = 2
+            页面大小, 就是每页显示几条(最后一页除外) 上面的参数 = 3
+
+            总条数, 在查询的时候就查出来了, 只不过没显示
+            总页数, 根据页面大小 和 总条数 进行计算. 总页数 = 总条数 / 页面大小 + 1
+            上一页下一页也好算.
+         */
+        PageInfo<User> userPageInfo = new PageInfo<User>(userList);
+        System.out.println("当前页码: " + userPageInfo.getPageNum());
+        System.out.println("每页显示的条数: " + userPageInfo.getPageSize());
+
+        System.out.println("总条数: " + userPageInfo.getTotal());
+        System.out.println("总页数: " + userPageInfo.getPages());
+        System.out.println("上一页: " + userPageInfo.getPrePage());
+        System.out.println("下一页: " + userPageInfo.getNextPage());
+
+        System.out.println("是否是第一页: " + userPageInfo.isIsFirstPage());
+        System.out.println("是否是最后一页: " + userPageInfo.isIsLastPage());
+
 
         // 关闭连接
         sqlSession.close();
