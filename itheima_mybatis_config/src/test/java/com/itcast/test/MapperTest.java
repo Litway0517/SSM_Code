2fedbe764b084b5aea9dced1e73b337133ae97d8
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-19 10:47:38 +0800

    mybatis_config 测试自定义的类日期转换类能否正常使用

---
+++ itheima_mybatis_config/src/test/java/com/itcast/test/MapperTest.java
@@ -0,0 +1,63 @@
+package com.itcast.test;
+
+import com.itcast.entity.User;
+import com.itcast.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+public class MapperTest {
+
+    @Test
+    public void test2() throws IOException {
+
+
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获得MyBatis框架生成的UserMapper接口的实现类
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = userMapper.findById(8);
+        System.out.println(user);
+        // 关闭连接
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void test() throws IOException {
+
+        User user = new User();
+        user.setUsername("jim");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获得MyBatis框架生成的UserMapper接口的实现类
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        /*
+            需求: 为了将Date类型转换为毫秒数存到数据库的,
+            数据库中的birthday字段类型是: bigint类型. (存入的数据类型 和 Mysql数据库中的字段类型不一样, 所以就报错了)
+
+            报错内容: Data truncated for column 'birthday' at row 1
+
+            因此需要自定义类型转换器
+         */
+        userMapper.save(user);
+
+        // 关闭连接
+        sqlSession.close();
+    }
+
+
+}
