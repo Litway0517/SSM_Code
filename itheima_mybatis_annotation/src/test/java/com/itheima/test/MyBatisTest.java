311302a00ea942e5c9a25a5276a5a876a5bec43e
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-20 13:54:56 +0800

    mybatis_multi 进行测试 @Before注解表示每次执行方法之前 先执行该注解下的方法

---
+++ itheima_mybatis_annotation/src/test/java/com/itheima/test/MyBatisTest.java
@@ -0,0 +1,72 @@
+package com.itheima.test;
+
+import com.itheima.entity.User;
+import com.itheima.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+public class MyBatisTest {
+
+    private UserMapper userMapper;
+
+    // @Before注解: 表示下面的测试方法执行之前会先执行这个方法
+    @Before
+    public void before() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        userMapper = sqlSession.getMapper(UserMapper.class);
+    }
+
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setUsername("tom");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        userMapper.save(user);
+    }
+
+    @Test
+    public void testUpdate() {
+        User user = new User();
+        user.setId(14);
+        user.setUsername("lihua");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        userMapper.update(user);
+    }
+
+    @Test
+    public void testDelete() {
+        userMapper.delete(13);
+    }
+
+    @Test
+    public void testFindById() {
+        User user = userMapper.findById(10);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<User> all = userMapper.findAll();
+        for (User user : all) {
+            System.out.println(user);
+        }
+    }
+
+
+
+
+}
