2120654977bee2e6f933dcdfdf170f67360f9b82
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-20 13:30:44 +0800

    mybatis_multi 一对多和多对多(User中有List<Role>成员变量): 主要使用<resultMap>和<collection property= ofType=>标签 property是成员变量的名称(待填充的数据) ofType是变量的类型(泛型)

--- itheima_mybatis_multi/src/main/java/com/itheima/mapper/UserMapper.java
+++ itheima_mybatis_multi/src/main/java/com/itheima/mapper/UserMapper.java
@@ -1,6 +1,16 @@
 package com.itheima.mapper;
 
+import com.itheima.entity.User;
+
+import java.util.List;
+
 public interface UserMapper {
+
+
+    public List<User> findAll();
+
+
+    public List<User> findAllUserAndRole();
 
 
 
