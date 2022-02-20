5e65607942a2ac908bd7422ef78dd2c22eefe2cd
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-20 13:51:19 +0800

    mybatis_multi xml文件进行CRUD操作 写接口然后写接口对应的xml文件 使用namespace连接

---
+++ itheima_mybatis_annotation/src/main/java/com/itheima/entity/User.java
@@ -0,0 +1,137 @@
+package com.itheima.entity;
+
+import java.util.Date;
+
+/**
+ * 用户
+ *
+ * @author DELL_
+ * @date 2022/02/17
+ */
+public class User {
+
+    /**
+     * id
+     */
+    private int id;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 生日
+     */
+    private Date birthday;
+
+
+    /**
+     * 用户
+     */
+    public User() {
+    }
+
+    /**
+     * 用户
+     *
+     * @param id       id
+     * @param username 用户名
+     * @param password 密码
+     */
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * 得到id
+     *
+     * @return int
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 组id
+     *
+     * @param id id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 获得用户名
+     *
+     * @return {@link String}
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置用户名
+     *
+     * @param username 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 得到密码
+     *
+     * @return {@link String}
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 得到生日
+     *
+     * @return {@link Date}
+     */
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * 设置生日
+     *
+     * @param birthday 生日
+     */
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * 字符串
+     *
+     * @return {@link String}
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
---
+++ itheima_mybatis_annotation/src/main/java/com/itheima/mapper/UserMapper.java
@@ -0,0 +1,51 @@
+package com.itheima.mapper;
+
+import com.itheima.entity.User;
+
+import java.util.List;
+
+/**
+ * 用户映射器
+ *
+ * @author DELL_
+ * @date 2022/02/19
+ */
+public interface UserMapper {
+
+    /**
+     * 保存
+     */
+    // @Insert(value = "insert into `user3` values(#{id},#{username},#{password},#{birthday})")
+    public void save(User user);
+
+    /**
+     * 更新
+     */
+    // @Update(value = "update `user3` set username=#{username},password=#{password},birthday=#{birthday} where id=#{id}")
+    public void update(User user);
+
+    /**
+     * 删除
+     */
+    // @Delete(value = "delete from `user3` where id=#{id}")
+    public void delete(int id);
+
+    /**
+     * 发现通过id
+     *
+     * @return {@link User}
+     */
+    // @Select(value = "select * from `user3` where id=#{id}")
+    public User findById(int id);
+
+    /**
+     * 找到所有
+     *
+     * @return {@link List}<{@link User}>
+     */
+    // @Select(value = "select * from `user3`")
+    public List<User> findAll();
+
+
+
+}
