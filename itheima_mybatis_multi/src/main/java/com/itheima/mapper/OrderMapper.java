085c971c58ad19b1c1e328cd9872b4ec4b14b4d6
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-20 13:24:11 +0800

    mybatis_multi 一对一关系(一个订单隶属于一个用户 Order中有一个User成员变量)查询所有订单: <resultMap>标签用来手动指明映射关系 <id>标签是结果中的主键映射 <result>标签是普通列映射 <association>标签用来指定映射结果到订单实体类中的用户成员变量

--- itheima_mybatis_multi/src/main/java/com/itheima/mapper/OrderMapper.java
+++ itheima_mybatis_multi/src/main/java/com/itheima/mapper/OrderMapper.java
@@ -1,7 +1,23 @@
 package com.itheima.mapper;
 
+import com.itheima.entity.Order;
+
+import java.util.List;
+
+/**
+ * 顺序映射器
+ *
+ * @author DELL_
+ * @date 2022/02/19
+ */
 public interface OrderMapper {
 
+    /**
+     * 找到所有
+     *
+     * @return {@link List}<{@link Order}>
+     */
+    public List<Order> findAll();
 
 
 
