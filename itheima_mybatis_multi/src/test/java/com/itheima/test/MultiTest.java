e58d4f3fce729c52e45efdfcdd692d95b68e6996
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-20 13:31:43 +0800

    mybatis_multi 测试: 一对一 一对多 多对多 这三种类型的查询

--- itheima_mybatis_multi/src/test/java/com/itheima/test/MultiTest.java
+++ itheima_mybatis_multi/src/test/java/com/itheima/test/MultiTest.java
@@ -1,7 +1,9 @@
 package com.itheima.test;
 
 import com.itheima.entity.Order;
+import com.itheima.entity.User;
 import com.itheima.mapper.OrderMapper;
+import com.itheima.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -15,7 +17,43 @@ import java.util.List;
 public class MultiTest {
 
 
+    // 多对多
+    @Test
+    public void test3() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获得MyBatis框架生成的UserMapper接口的实现类
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 
+        List<User> allUserAndRole = mapper.findAllUserAndRole();
+        for (User user : allUserAndRole) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+
+    }
+
+    // 一对多
+    @Test
+    public void test2() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获得MyBatis框架生成的UserMapper接口的实现类
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        List<User> userList = mapper.findAll();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+
+    }
+
+
+    // 一对一
     @Test
     public void test() throws IOException {
         InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
