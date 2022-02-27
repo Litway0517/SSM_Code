870b6c59f150f05fc4e46d4c5045280a314fc084
Mingw0717-Dell <xmw0517@gmail.com>
2022-02-27 21:48:40 +0800

    mybatis_multiple 复习一对一查询: 需要用<association property="" javaType="">

--- itheima_mybatis_multiple/src/main/java/com/itheima/mapper/UserMapper.java
+++ itheima_mybatis_multiple/src/main/java/com/itheima/mapper/UserMapper.java
@@ -4,12 +4,28 @@ import com.itheima.entity.User;
 
 import java.util.List;
 
+/**
+ * 用户映射器
+ *
+ * @author DELL_
+ * @date 2022/02/27
+ */
 public interface UserMapper {
 
 
+    /**
+     * 查询所有
+     *
+     * @return {@link List}<{@link User}>
+     */
     public List<User> findAll();
 
 
+    /**
+     * 找到所有用户和角色
+     *
+     * @return {@link List}<{@link User}>
+     */
     public List<User> findAllUserAndRole();
 
 
